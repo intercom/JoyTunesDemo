abd609c54a45e164f6c560b72f877aa9a9994f08
bboyle18 <bboyle18@gmail.com>
2022-01-26 16:17:10 +0000

    Demo app changes

--- JoyTunesDemo/AppDelegate.swift
+++ JoyTunesDemo/AppDelegate.swift
@@ -10,7 +10,7 @@ import Intercom
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
     var intercomIsOpen: Bool = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
